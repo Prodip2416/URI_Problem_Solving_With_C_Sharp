8c92c8a271893619655449995cc2572158a21beb
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-08-21 09:56:44 +0600

    C#

---
+++ Beginner/1564/1564/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1564
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n == 0)
+                    Console.WriteLine("vai ter copa!");
+                else if (n > 0)
+                    Console.WriteLine("vai ter duas!");
+                else
+                    return;
+            }
+        }
+    }
+}
---
+++ Beginner/1759/1759/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1759
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write("Ho ");
+            }
+         
+            Console.Write("Ho!\n");
+           
+        }
+    }
+}
