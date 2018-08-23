a1c4a673222892b1f793e905d8c49e99fcd18031
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-08-23 19:13:28 +0600

    C#

---
+++ Beginner/1866/1866/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1866
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                if (b % 2 == 0)
+                    Console.WriteLine("0");
+                else
+                {
+                    Console.WriteLine("1");
+                }
+            }
+        }
+    }
+}
---
+++ Beginner/1921/1921/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1921
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong a = Convert.ToUInt64(Console.ReadLine());
+
+            ulong b = (a*(a - 3))/2;
+            Console.WriteLine(b);
+        }
+    }
+}
